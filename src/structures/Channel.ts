ae3b730c2e33e2441443ae5d9f3e47006cd26d59
VincentJonathan <vinjongm@gmail.com>
2020-12-25 16:51:19 +0700

    update comment

--- src/structures/Channel.ts
+++ src/structures/Channel.ts
@@ -1,7 +1,7 @@
 import Client from "../client";
 import Base from "./Base";
 
-/** Represents a Base class that will be inherited by all other structure classes. */
+/** Represents a chat channel. */
 export default class Channel extends Base {
 	/**
 	 * The channel name
